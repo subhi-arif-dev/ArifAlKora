a488c00edff71ab7818885a59d537712c2308104
subhi-arif-dev <sophi.baraah3@gmail.com>
2026-06-14 11:35:08 +0300

    Update MainActivity.kt

--- app/src/main/java/com/subhi/arifalkora/MainActivity.kt
+++ app/src/main/java/com/subhi/arifalkora/MainActivity.kt
@@ -17,11 +17,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 1. تهيئة المحرك (المستودع) والمدير الفني بمجرد فتح التطبيق
         val repository = QuestionRepository(this)
         val viewModel = GameViewModel(repository)
 
-        // 2. أمر التحميل: سنقوم بتحميل مستوى "الأسئلة السهلة" كبداية تلقائية
         viewModel.loadLevel("easy_questions.json")
 
         setContent {
@@ -30,13 +28,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // 3. مراقبة تدفق البيانات من المدير الفني
                     val questions by viewModel.questions.collectAsState()
                     val currentIndex by viewModel.currentQuestionIndex.collectAsState()
 
-                    // 4. إذا تم تحميل الأسئلة بنجاح، قم بعرض الشاشة واسحب السؤال الحالي
                     if (questions.isNotEmpty()) {
-                        GameScreen(question = questions[currentIndex])
+                        GameScreen(
+                            question = questions[currentIndex],
+                            onNextQuestion = { isCorrect ->
+                                // بمجرد مرور الثانيتين في شاشة اللعب، يتم استدعاء هذا الأمر
+                                viewModel.answerQuestion(isCorrect)
+                            }
+                        )
                     }
                 }
             }
