2cb6861db52c5bf8ba443d90c7c3e73b06b9ef40
subhi-arif-dev <sophi.baraah3@gmail.com>
2026-06-14 11:02:31 +0300

    Create Color.kt

---
+++ app/src/main/java/com/subhi/arifalkora/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.subhi.arifalkora.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// الألوان الملوكية لتطبيق عارف الكورة
+val RoyalGreen = Color(0xFF0A5C36) // أخضر زمردي للدرع
+val DarkBackground = Color(0xFF121212) // أسود مطفي نقي للخلفية
+val GoldAccent = Color(0xFFFFD700) // ذهبي مشع للكؤوس والنصوص المهمة
+val GlassBackground = Color(0x33FFFFFF) // لون زجاجي شفاف للحاويات
+val ErrorRed = Color(0xFFD32F2F) // أحمر ياقوتي للإجابات الخاطئة
