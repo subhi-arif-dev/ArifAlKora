8cc4feb42d581626b9068b0319dc4b2d02fa34da
subhi-arif-dev <sophi.baraah3@gmail.com>
2026-06-15 14:14:46 +0300

    Create BannerAd.kt

---
+++ app/src/main/java/com/subhi/arifalkora/ui/components/BannerAd.kt
@@ -0,0 +1,26 @@
+package com.subhi.arifalkora.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+
+@Composable
+fun BannerAd(modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier.fillMaxWidth().background(Color.Black),
+        factory = { context ->
+            AdView(context).apply {
+                setAdSize(AdSize.BANNER)
+                // هذا هو الرقم التجريبي الرسمي للبانر من جوجل
+                adUnitId = "ca-app-pub-3940256099942544/6300978111"
+                loadAd(AdRequest.Builder().build())
+            }
+        }
+    )
+}
