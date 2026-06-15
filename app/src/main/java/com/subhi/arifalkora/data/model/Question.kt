08967db554ef47676103fbac455ab64d8e2a7dc0
subhi-arif-dev <sophi.baraah3@gmail.com>
2026-06-15 15:36:39 +0300

    Update Question.kt

--- app/src/main/java/com/subhi/arifalkora/data/model/Question.kt
+++ app/src/main/java/com/subhi/arifalkora/data/model/Question.kt
@@ -1,9 +1,12 @@
 package com.subhi.arifalkora.data.model
 
+import androidx.annotation.Keep
+
 /**
  * هذا هو القالب الذي سيقوم بتحويل نصوص الـ JSON إلى كائنات برمجية (Objects)
- * لاحظ أن أسماء المتغيرات هنا تطابق تماماً الأسماء الموجودة في ملفات JSON الخاصة بك
+ * كلمة @Keep تمنع تشفير هذا القالب في النسخة النهائية لكي لا تتعطل الأسئلة
  */
+@Keep
 data class Question(
     val id: Int,
     val level: String,
